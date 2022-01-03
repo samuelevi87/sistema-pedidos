2a90b353bd5a00ec8fc0696066534763e70b614c
samuelevi87 <samuel.levi.alves@outlook.com>
2022-01-03 13:22:35 -0300

    "Testando a primeira classe de domínio - Categoria"

--- src/main/java/com/slevi/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/slevi/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.slevi.cursomc.resources;
 
+import com.slevi.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST funcionando!";
+    public List<Categoria> listar(){
+
+        Categoria ctg1 = new Categoria(1, "Informática");
+        Categoria ctg2 = new Categoria(2, "Escritório");
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(ctg1);
+        lista.add(ctg2);
+
+        return lista;
     }
 }
